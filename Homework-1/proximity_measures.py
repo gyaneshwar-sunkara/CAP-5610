226cd1129073b64498f501ce44e0bb8fd579aedb
Gyani Sunkara <gyanisunkara1@gmail.com>
2023-09-17 23:46:21 -0400

    Homework 1

---
+++ Homework-1/proximity_measures.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+def euclidean_distance(v1, v2):
+    """
+    Euclidean Distance - L2 Norm
+
+    Args:
+        v1: The first vector
+        v2: The second vector
+
+    Returns:
+        Euclidean distance between v1 and v2
+
+    """
+    difference = v1 - v2
+
+    difference_squared = difference * difference
+
+    summation = np.sum(difference_squared)
+
+    euclidean_distance = np.sqrt(summation)
+
+    return euclidean_distance
+
+    # return np.linalg.norm(v1 - v2)
+
+
+def cosine_similarity(v1, v2):
+    """
+    Cosine Similarity
+
+    Args:
+        v1: The first vector.
+        v2: The second vector.
+
+    Returns:
+        Cosine similarity of v1 and v2.
+    """
+    v1_length = np.linalg.norm(v1)
+    v2_length = np.linalg.norm(v2)
+
+    v1_dot_v2 = np.dot(v1, v2)
+
+    cosine_similarity = v1_dot_v2 / (v1_length * v2_length)
+
+    return cosine_similarity
+
+
+def correlation(v1, v2):
+    """
+    Pearson's correlation
+
+    Args:
+        v1: The first vector
+        v2: The second vector
+
+    Returns:
+        Correlation between v1 and v2
+    """
+    
+    return np.corrcoef(v1, v2)[0, 1]
+
+
+if __name__ == "__main__":
+    np.random.seed(1)
+
+    v1 = np.random.randint(0, 20, 10)
+    v2 = np.random.randint(0, 20, 10)
+    print("Euclidean Distance between two vectors: ", euclidean_distance(v1, v2))
+
+    doc_v1 = np.array([3, 2, 0, 5, 0, 0, 0, 2, 0, 0])
+    doc_v2 = np.array([1, 0, 0, 0, 0, 0, 0, 1, 0, 2])
+    print("Cosine similarity of two document vectors: ",
+          cosine_similarity(doc_v1, doc_v2))
+
+    print("Pearson's correlation between two vectors: ", correlation(v1, v2))
---
+++ Homework-1/proximity_measures_bonus.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy.spatial import distance
+from scipy import stats
+import pandas as pd
+
+
+def euclidean_distance_scipy(v1, v2):
+    return distance.euclidean(v1, v2)
+
+
+def cosine_similarity_scipy(v1, v2):
+    return 1 - distance.cosine(v1, v2)
+
+
+def correlation_scipy(v1, v2):
+    return stats.pearsonr(v1, v2).statistic
+
+
+def correlation_pandas(v1, v2):
+    df = pd.DataFrame({'v1': v1, 'v2': v2})
+    correlation = df.corr(method="pearson")["v1"]["v2"]
+    return correlation
+
+
+if __name__ == "__main__":
+    np.random.seed(1)
+
+    v1 = np.random.randint(0, 20, 10)
+    v2 = np.random.randint(0, 20, 10)
+    print("Eculidean Distance between two vectors using scipy: ",
+          euclidean_distance_scipy(v1, v2))
+
+    doc_v1 = np.array([3, 2, 0, 5, 0, 0, 0, 2, 0, 0])
+    doc_v2 = np.array([1, 0, 0, 0, 0, 0, 0, 1, 0, 2])
+    print("Cosine similarity of two document vectors using scipy: ",
+          cosine_similarity_scipy(doc_v1, doc_v2))
+
+    print("Pearson's correlation between two vectors using scipy: ",
+          correlation_scipy(v1, v2))
+    print("Pearson's correlation between two vectors using pandas: ",
+          correlation_pandas(v1, v2))
